064788105838e75da8c307c6f6e5622c99a788db
Mxliihxx <malihaaishaaq@gmail.com>
2025-04-09 06:32:28 +0530

    refactor(models): rename players.ts to players.ts

--- models/Players.ts
+++ models/Players.ts
@@ -1,41 +1,41 @@
 import mongoose from "mongoose";
 
 const playerSchema = new mongoose.Schema({
-    playerName :{
-        type: String,
-        required : true,
-    },
-    playerId : {
-        type: String,
-        required: true,
-        Unique : true,
-    },
-    rating : {
-        type : Number,
-        required : true,
-    },
-    boughtAt : {
-        type : Number,
-    },
-    isSold : {
-        type : Boolean,
-        required : false,
-    },
-    role : {
-        type: String,
-        // required : true,
-    },
-    nationality : {
-        type: String,
-        // required : true,
-    }, 
-    basePrice : {
-        type: String,
-    },
-    pool : {
-        type: Number,
-        required: true,
-    },
+  playerName: {
+    type: String,
+    required: true,
+  },
+  playerId: {
+    type: String,
+    required: true,
+    Unique: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  boughtAt: {
+    type: Number,
+  },
+  isSold: {
+    type: Boolean,
+    required: false,
+  },
+  role: {
+    type: String,
+    // required : true,
+  },
+  nationality: {
+    type: String,
+    // required : true,
+  },
+  basePrice: {
+    type: String,
+  },
+  pocket: {
+    type: Number,
+    required: true,
+  },
 });
 
-export default mongoose.model("player",playerSchema);
+export default mongoose.model("player", playerSchema);
